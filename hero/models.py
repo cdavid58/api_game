740d88dcd704be413c766660012490b9b9604404
cdavid58 <carlos_david753@hotmail.com>
2023-08-08 20:07:12 -0500

    p

--- hero/models.py
+++ hero/models.py
@@ -1,27 +1,32 @@
 from django.db import models
 
 class Rol(models.Model):
-	name = models.CharField(max_length= 35)
+    name = models.CharField(max_length=35)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
 class Skill(models.Model):
-	name = models.CharField(max_length = 150)
-	additional_damage = models.IntegerField()
+    name = models.CharField(max_length=150)
+    description = models.TextField()  
+    additional_damage = models.IntegerField()
 
-	def __str__(self):
-		return self.name
-
+    def __str__(self):
+        return self.name
 
 class Hero(models.Model):
-	hp = models.IntegerField()
-	physical_damage = models.IntegerField()
-	magic_damage = models.IntegerField()
-	attack_speed = models.IntegerField()
-	armor = models.IntegerField()
-	magic_resist = models.IntegerField()
-	movement_speed = models.IntegerField()
-	skill = models.ManyToManyField(Skill)
-	rol = models.ForeignKey(Rol,on_delete=models.CASCADE)
+    hp = models.PositiveIntegerField()
+    physical_damage = models.PositiveIntegerField()
+    magic_damage = models.PositiveIntegerField()
+    attack_speed = models.FloatField()  
+    armor = models.PositiveIntegerField()
+    magic_resist = models.PositiveIntegerField()
+    movement_speed = models.PositiveIntegerField()
+    skills = models.ManyToManyField(Skill, blank=True)  
+    rol = models.ForeignKey(Rol, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.rol} - HP: {self.hp}"
+
+    class Meta:
+        ordering = ['rol']  
